6f0377211b8f7401a2b637ed39364c43856a27e3
Paolo <186249117+PaoloESAN@users.noreply.github.com>
2026-01-09 22:18:51 -0500

    feat: meta tag y logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
 	import "./layout.css";
-	import favicon from "$lib/assets/favicon.svg";
+	import favicon from "$lib/assets/favicon.png";
 	import { ModeWatcher } from "mode-watcher";
 	let { children } = $props();
 </script>
 
 <ModeWatcher />
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<title>Anware Store</title>
+	<meta
+		name="description"
+		content="Tu tienda de confianza para productos de alta calidad."
+	/>
+	<meta property="og:title" content="Anware Store" />
+	<meta property="og:type" content="website" />
+	<link rel="icon" href={favicon} />
+</svelte:head>
 {@render children()}
